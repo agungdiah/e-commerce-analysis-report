26b7496790416f7da4a74d99cc15f4af8b810907
agungdiah <121778260+agungdiah@users.noreply.github.com>
2024-01-09 07:50:47 +0800

    dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -7,7 +7,7 @@ import streamlit as st
 from babel.numbers import format_currency
 sns.set(style="dark")
 
-all_df = pd.read_csv("https://github.com/agungdiah/e-commerce-analysis-report/blob/master/dashboard/dashboard.py")
+all_df = pd.read_csv("https://raw.githubusercontent.com/agungdiah/e-commerce-analysis-report/master/dashboard/main_data.csv")
 
 # membuat header
 st.header('Olist Store')
