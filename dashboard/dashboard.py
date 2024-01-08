1193ba3275f88e9a1ceffda2b0c9c63bb4afca86
agungdiah <121778260+agungdiah@users.noreply.github.com>
2024-01-09 01:11:58 +0800

    dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,7 +1,8 @@
 # import library
 
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot
+ as plt
 import seaborn as sns
 import streamlit as st
 from babel.numbers import format_currency
